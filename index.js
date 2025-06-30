7a942e7a2b20dbc2c64b9926cde30b8d92839875
DulithKavinda <121927516+DulithKavinda@users.noreply.github.com>
2025-06-30 12:01:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 300;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// Route for the home page
+// Route to the home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
